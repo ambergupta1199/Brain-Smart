d9352d7949f839816e95bce91339526cb9b27dd6
hp <hp@LAPTOP-5RQIEBP6.local>
2021-03-03 21:27:01 +0530

    Updates

---
+++ precache-manifest.2d89f98761f64072fda7601797a60099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da8c0a717faf983a18033df8726c651",
+    "url": "/Brain-Smart/index.html"
+  },
+  {
+    "revision": "e85086beccbfd3505394",
+    "url": "/Brain-Smart/static/css/2.24daf3d1.chunk.css"
+  },
+  {
+    "revision": "df33a5d8130f99f8f767",
+    "url": "/Brain-Smart/static/css/main.6e39c12d.chunk.css"
+  },
+  {
+    "revision": "e85086beccbfd3505394",
+    "url": "/Brain-Smart/static/js/2.96cf6fbe.chunk.js"
+  },
+  {
+    "revision": "df33a5d8130f99f8f767",
+    "url": "/Brain-Smart/static/js/main.82b0c4d5.chunk.js"
+  },
+  {
+    "revision": "6beeb0b349644caeabde",
+    "url": "/Brain-Smart/static/js/runtime-main.ffd4cb19.js"
+  }
+]);
