4fd203a7052031e124cd33a61ad3a30f045eb317
hp <hp@LAPTOP-5RQIEBP6.local>
2021-03-03 21:39:44 +0530

    Updates

--- precache-manifest.ff77b0abd7f0838bbea11896a49f7311.js
+++ precache-manifest.ff77b0abd7f0838bbea11896a49f7311.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4da8c0a717faf983a18033df8726c651",
+    "revision": "1ab1d9fba87967b26bd34502c55ba763",
     "url": "/Brain-Smart/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Brain-Smart/static/css/2.24daf3d1.chunk.css"
   },
   {
-    "revision": "df33a5d8130f99f8f767",
+    "revision": "2866dd5f8ba7970ad148",
     "url": "/Brain-Smart/static/css/main.6e39c12d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Brain-Smart/static/js/2.96cf6fbe.chunk.js"
   },
   {
-    "revision": "df33a5d8130f99f8f767",
-    "url": "/Brain-Smart/static/js/main.82b0c4d5.chunk.js"
+    "revision": "2866dd5f8ba7970ad148",
+    "url": "/Brain-Smart/static/js/main.9982adbf.chunk.js"
   },
   {
     "revision": "6beeb0b349644caeabde",
